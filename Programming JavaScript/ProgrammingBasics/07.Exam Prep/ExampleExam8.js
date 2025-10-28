87f6c6efbbbc918eb89a82e93d77f96e9315aeb0
kdamianov <kdamianov@gmail.com>
2025-10-28 22:49:20 +0200

    Fixed commented lines

--- Programming JavaScript/ProgrammingBasics/07.Exam Prep/ExampleExam8.js
+++ Programming JavaScript/ProgrammingBasics/07.Exam Prep/ExampleExam8.js
@@ -1,145 +1,146 @@
-// //1. Fruit Market
-// function execute(
-//   strawberryPrice,
-//   bananasQty,
-//   orangesQty,
-//   raspberriesQty,
-//   strawberriesQty
-// ) {
-//   let raspberryPrice = strawberryPrice / 2;
-//   let orangePrice = raspberryPrice * 0.6;
-//   let bananaPrice = raspberryPrice * 0.2;
+//1. Fruit Market
 
-//   let strawberries = strawberriesQty * strawberryPrice;
-//   let raspberries = raspberriesQty * raspberryPrice;
-//   let oranges = orangesQty * orangePrice;
-//   let bananas = bananasQty * bananaPrice;
+function execute(
+  strawberryPrice,
+  bananasQty,
+  orangesQty,
+  raspberriesQty,
+  strawberriesQty
+) {
+  let raspberryPrice = strawberryPrice / 2;
+  let orangePrice = raspberryPrice * 0.6;
+  let bananaPrice = raspberryPrice * 0.2;
 
-//   let totalAmount = strawberries + raspberries + oranges + bananas;
+  let strawberries = strawberriesQty * strawberryPrice;
+  let raspberries = raspberriesQty * raspberryPrice;
+  let oranges = orangesQty * orangePrice;
+  let bananas = bananasQty * bananaPrice;
 
-//   console.log(totalAmount.toFixed(2));
-// }
+  let totalAmount = strawberries + raspberries + oranges + bananas;
 
-// execute(48, 10, 3.3, 6.5, 1.7);
+  console.log(totalAmount.toFixed(2));
+}
 
-// //2. Safari
-// function execute(budget, fuel, day) {
-//   let fuelMoney = Number(fuel) * 2.1;
-//   let totalMoney = fuelMoney + 100;
+execute(48, 10, 3.3, 6.5, 1.7);
 
-//   switch (day) {
-//     case "Saturday":
-//       totalMoney *= 0.9;
-//       break;
-//     case "Sunday":
-//       totalMoney *= 0.8;
-//       break;
-//   }
-//   let moneyDiff = 0;
-//   if (totalMoney <= budget) {
-//     moneyDiff = budget - totalMoney;
-//     console.log(`Safari time! Money left: ${moneyDiff.toFixed(2)} lv. `);
-//   } else {
-//     moneyDiff = totalMoney - budget;
-//     console.log(`Not enough money! Money needed: ${moneyDiff.toFixed(2)} lv.`);
-//   }
-// }
+//2. Safari
+function execute(budget, fuel, day) {
+  let fuelMoney = Number(fuel) * 2.1;
+  let totalMoney = fuelMoney + 100;
 
-// execute(1000, 10, 'Sunday');
+  switch (day) {
+    case "Saturday":
+      totalMoney *= 0.9;
+      break;
+    case "Sunday":
+      totalMoney *= 0.8;
+      break;
+  }
+  let moneyDiff = 0;
+  if (totalMoney <= budget) {
+    moneyDiff = budget - totalMoney;
+    console.log(`Safari time! Money left: ${moneyDiff.toFixed(2)} lv. `);
+  } else {
+    moneyDiff = totalMoney - budget;
+    console.log(`Not enough money! Money needed: ${moneyDiff.toFixed(2)} lv.`);
+  }
+}
 
-// //3. Mobile operator
-// function execute(years, type, internet, months) {
-//   let tax = 0;
-//   switch (years) {
-//     case "one":
-//       if (type === "Small") {
-//         tax = 9.98;
-//       } else if (type === "Middle") {
-//         tax = 18.99;
-//       } else if (type === "Large") {
-//         tax = 25.98;
-//       } else {
-//         tax = 35.99;
-//       }
-//       break;
-//     case "two":
-//       if (type === "Small") {
-//         tax = 8.58;
-//       } else if (type === "Middle") {
-//         tax = 17.09;
-//       } else if (type === "Large") {
-//         tax = 23.59;
-//       } else {
-//         tax = 31.79;
-//       }
-//       break;
-//   }
+execute(1000, 10, "Sunday");
 
-//   if (internet === "yes") {
-//     if (tax <= 10) {
-//       tax += 5.5;
-//     } else if (tax <= 30) {
-//       tax += 4.35;
-//     } else {
-//       tax += 3.85;
-//     }
-//   }
+//3. Mobile operator
+function execute(years, type, internet, months) {
+  let tax = 0;
+  switch (years) {
+    case "one":
+      if (type === "Small") {
+        tax = 9.98;
+      } else if (type === "Middle") {
+        tax = 18.99;
+      } else if (type === "Large") {
+        tax = 25.98;
+      } else {
+        tax = 35.99;
+      }
+      break;
+    case "two":
+      if (type === "Small") {
+        tax = 8.58;
+      } else if (type === "Middle") {
+        tax = 17.09;
+      } else if (type === "Large") {
+        tax = 23.59;
+      } else {
+        tax = 31.79;
+      }
+      break;
+  }
 
-//   let ttlAmount = tax * months;
-//   if (years === "two") {
-//     ttlAmount -= ttlAmount * 0.0375;
-//   }
+  if (internet === "yes") {
+    if (tax <= 10) {
+      tax += 5.5;
+    } else if (tax <= 30) {
+      tax += 4.35;
+    } else {
+      tax += 3.85;
+    }
+  }
 
-//   console.log(`${ttlAmount.toFixed(2)} lv.`);
-// }
+  let ttlAmount = tax * months;
+  if (years === "two") {
+    ttlAmount -= ttlAmount * 0.0375;
+  }
 
-// execute("two", "Large", "no", 10);
+  console.log(`${ttlAmount.toFixed(2)} lv.`);
+}
 
-// //5. Tourist Shop
-// function execute(input) {
-//   let budget = Number(input[0]);
-//   let index = 1;
-//   let command = input[index];
-//   let productCounter = 0;
-//   let sumProducts = 0;
-//   while (command !== "Stop") {
-//     let product = input[index++];
+execute("two", "Large", "no", 10);
 
-//     let productPrice = Number(input[index]);
-//     productCounter++;
-//     if (productCounter % 3 === 0) {
-//       productPrice = productPrice / 2;
-//     }
-//     sumProducts += productPrice;
-//     budget = budget - productPrice;
-//     if (budget < 0) {
-//       console.log("You don't have enough money!");
-//       console.log(`You need ${Math.abs(budget).toFixed(2)} leva!`);
-//       break;
-//     }
+//5. Tourist Shop
+function execute(input) {
+  let budget = Number(input[0]);
+  let index = 1;
+  let command = input[index];
+  let productCounter = 0;
+  let sumProducts = 0;
+  while (command !== "Stop") {
+    let product = input[index++];
 
-//     index++;
-//     command = input[index];
-//   }
-//   if (command === "Stop") {
-//     console.log(
-//       `You bought ${productCounter} products for ${sumProducts.toFixed(
-//         2
-//       )} leva.`
-//     );
-//   }
-// }
+    let productPrice = Number(input[index]);
+    productCounter++;
+    if (productCounter % 3 === 0) {
+      productPrice = productPrice / 2;
+    }
+    sumProducts += productPrice;
+    budget = budget - productPrice;
+    if (budget < 0) {
+      console.log("You don't have enough money!");
+      console.log(`You need ${Math.abs(budget).toFixed(2)} leva!`);
+      break;
+    }
 
-// execute([
-//   "153.20",
-//   "Backpack",
-//   "25.20",
-//   "Shoes",
-//   "54",
-//   "Sunglasses",
-//   "30 ",
-//   "Stop",
-// ]);
+    index++;
+    command = input[index];
+  }
+  if (command === "Stop") {
+    console.log(
+      `You bought ${productCounter} products for ${sumProducts.toFixed(
+        2
+      )} leva.`
+    );
+  }
+}
+
+execute([
+  "153.20",
+  "Backpack",
+  "25.20",
+  "Shoes",
+  "54",
+  "Sunglasses",
+  "30 ",
+  "Stop",
+]);
 
 //6. Vet Parking
 function execute(days, hours) {
@@ -160,7 +161,7 @@ function execute(days, hours) {
     ttlPrice += dailyPrice;
     console.log(`Day: ${day} - ${dailyPrice.toFixed(2)} leva`);
   }
-  console.log(`Total: ${ttlPrice} leva`);
+  console.log(`Total: ${ttlPrice.toFixed(2)} leva`);
 }
 
 execute(2, 5);
